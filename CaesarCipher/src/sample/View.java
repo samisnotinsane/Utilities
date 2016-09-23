64f09d61fb19d1d459b34d038f02a7112e740ebf
Sameen Islam <ec14370@qmul.ac.uk>
2016-09-23 22:04:19 +0100

    Created GUI.

---
+++ CaesarCipher/src/sample/View.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+/**
+ * Created by sameenislam on 23/09/2016.
+ */
+public class View {
+
+
+
+}
